80aed847973ed0fc8381a8d1d3c3837692da9982
Аброрбек <abrorbek@MacBook-Air-Abrorbek.local>
2023-06-20 21:26:55 +0600

    Added custom textView

---
+++ ToDoList/ToDoList/Components/CustomTextView.swift
@@ -0,0 +1,59 @@
+//
+//  CustomTextView.swift
+//  ToDoList
+//
+//  Created by Аброрбек on 20.06.2023.
+//
+
+import UIKit
+
+final class CustomTextView: UIView {
+    
+    init(delegate: UITextViewDelegate){
+        super.init(frame: .zero)
+        textView.delegate = delegate
+        
+        setupTextView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @IBInspectable var keyboardType: UIKeyboardType = .default {
+        didSet {
+            textView.keyboardType = keyboardType
+        }
+    }
+    
+    private func setupTextView(){
+        self.backgroundColor = .white //change later
+        self.addSubview(textView)
+        setupLayout()
+    }
+    
+    private func setupLayout(){
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            textView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
+            textView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            textView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        textView.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        textView.backgroundColor = .white
+        textView.layer.cornerRadius = 16.0
+        textView.textAlignment = .left
+        textView.sizeToFit()
+        textView.isScrollEnabled = false
+        textView.text = "Что надо сделать?"
+        textView.textColor = UIColor.lightGray
+        
+        return textView
+    }()
+}
---
+++ ToDoList/ToDoList/Resources/UIViewController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Extension.swift
+//  ToDoList
+//
+//  Created by Аброрбек on 20.06.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
