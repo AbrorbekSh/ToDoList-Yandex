e6caa29c52b3f2fc6a30a8ba20c4fcec7dbbb58f
Аброрбек <abrorbek@MacBook-Air-Abrorbek.local>
2023-06-29 18:34:51 +0600

    Added coordinator

---
+++ ToDoList/ToDoList/Coordinators/AppCoordinator.swift
@@ -0,0 +1,35 @@
+//
+//  AppCoordinator.swift
+//  ToDoList
+//
+//  Created by Аброрбек on 29.06.2023.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var childCoordinators: [Coordinator] { get }
+    func start()
+}
+
+final class AppCoordinator: Coordinator {
+    
+    private(set) var childCoordinators: [Coordinator] = []
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow){
+        self.window = window
+    }
+    
+    func start() {
+        let navigationController = UINavigationController()
+        
+        let toDoItemsListCoordinator = ToDoItemListCoordinator(navigationController: navigationController)
+        childCoordinators.append(toDoItemsListCoordinator)
+        toDoItemsListCoordinator.start()
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
